98e3d0532f0bbd3c085a2da4396a4eb60ae8f5f1
ke7chyp <nekitos199@gmail.com>
2021-07-12 08:45:30 +0300

    bug fixes

--- books_parser/books_parser/spiders/knijki_parser.py
+++ books_parser/books_parser/spiders/knijki_parser.py
@@ -1,18 +1,20 @@
 from bs4 import BeautifulSoup
 import scrapy
-AUTHOR = 'lev-tolstoy'
 
-class ExampleSpider(scrapy.Spider):
+
+class BooksParser(scrapy.Spider):
     name = "spider"
 
-    def start_reqsuests(self):
-        last_page = True
-        page_num = 0
-        while last_page:
-            url = f"https://knijky.ru/authors/{AUTHOR}?page={page_num}"
-            page_num += 1
-            yield scrapy.Request(url=url, callback=self.parse)
-            last_page = scrapy.Request(url=url, callback=self.parse).response.status == 200
+    def start_requests(self):
+        for author in ("lev-tolstoy", "fedor-dostoevskiy", "mihail-bulgakov"):
+            self.last_page = True
+            page_num = 0
+            while self.last_page:
+                page_num += 1
+                yield scrapy.Request(
+                    url=f"https://knijky.ru/authors/{author}?page={page_num}",
+                    callback=self.parse,
+                )
 
     def parse(self, response):
         self.soup = BeautifulSoup(response.text, "lxml")
@@ -22,5 +24,9 @@ class ExampleSpider(scrapy.Spider):
             current_element = part_of_page.find(
                 "span", {"class": "field-content"}
             ).find("a")
-
-            yield {"text": current_element.text, "href": current_element.get("href")}
+            yield {
+                "text": current_element.text,
+                "href": current_element.get("href"),
+            }
+        if response.status == 404:
+            self.last_page = False
